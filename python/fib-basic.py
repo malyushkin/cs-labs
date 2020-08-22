2b53ec53b9c1aa860f6c5aeee80f0b82017a2d35
Roman <malyushkinr@gmail.com>
2020-08-22 17:36:09 +0300

    fibonacci ex added

--- python/fib-basic.py
+++ python/fib-basic.py
@@ -7,34 +7,35 @@ from pprint import pprint
 
 
 def memoize(func):
-	cache = dict()
-	def wrapper(n, *args):
-		if n not in cache:
-			cache[n] = func(n)
-			pprint(dict(func=func.__name__, result=cache[n], n=n, args=args))
-		return cache[n]
-	return wrapper
+    cache = dict()
+
+    def wrapper(n, *args):
+        if n not in cache:
+            cache[n] = func(n)
+            pprint(dict(func=func.__name__, result=cache[n], n=n, args=args))
+        return cache[n]
+    return wrapper
 
 
 @memoize
 def fib_recursive(n):
-	if n <= 1:
-		return n
-	return fib_recursive(n-1) + fib_recursive(n-2)
+    if n <= 1:
+        return n
+    return fib_recursive(n-1) + fib_recursive(n-2)
 
 
 @memoize
 def fib_arr(n):
-	f0, f1 = 0, 1
-	for i in range(1, n):
-		f0, f1 = f1, f0 + f1
-	return f1
+    f0, f1 = 0, 1
+    for i in range(1, n):
+        f0, f1 = f1, f0 + f1
+    return f1
 
 
 def main():
-	fib_recursive(15)
-	fib_arr(20)
+    fib_recursive(15)
+    fib_arr(20)
 
 
 if __name__ == '__main__':
-	main()
+    main()
